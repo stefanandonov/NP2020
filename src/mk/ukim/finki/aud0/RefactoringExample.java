186b7902121b9b8813ffcdb4d67c7002b5becedf
atodorovska <ana5todorovska@gmail.com>
2020-10-16 01:08:18 +0200

    exercise 0 and 1 for Friday group - Ana

--- src/mk/ukim/finki/aud0/RefactoringExample.java
+++ src/mk/ukim/finki/aud0/RefactoringExample.java
@@ -2,20 +2,23 @@ package mk.ukim.finki.aud0;
 
 public class RefactoringExample {
 
-    int countAllNumbersDivisibleWithDigitsSum (int start, int end) {
+    public int countAllNumbersDivisibleWithDigitsSum(int start, int end){
         int count = 0;
-        for (int i=start; i<=end; i++) {
-            if (i%getSumOfDigits(i)==0)
-                ++count;
+
+        for(int i=start; i<=end; i++){
+            if(i % getSumOfDigits(i) == 0)
+                count++;
         }
+
         return count;
     }
 
+    //a method can be extracted using Refactor -> Extract Method (on a selected code snippet)
     private int getSumOfDigits(int temp) {
         int sumOfDigits = 0;
-        while (temp >0) {
-            sumOfDigits+=(temp %10);
-            temp /=10;
+        while(temp > 0){
+            sumOfDigits += (temp %10);
+            temp /= 10;
         }
         return sumOfDigits;
     }
